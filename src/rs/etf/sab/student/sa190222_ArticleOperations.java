8f4728d89d3a59cba6072142d26336b73864f2f1
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-05-22 22:57:06 +0200

    Added createArticle method in sa190222_ArticleOperations

--- src/rs/etf/sab/student/sa190222_ArticleOperations.java
+++ src/rs/etf/sab/student/sa190222_ArticleOperations.java
@@ -4,6 +4,9 @@
  */
 package rs.etf.sab.student;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import rs.etf.sab.operations.ArticleOperations;
 
 /**
@@ -13,8 +16,68 @@ import rs.etf.sab.operations.ArticleOperations;
 public class sa190222_ArticleOperations implements ArticleOperations {
 
     @Override
-    public int createArticle(int i, String string, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int createArticle(int shopId, String articleName, int articlePrice) {
+        int ret = -1;
+        Connection conn = DB.getInstance().getConnection();
+        
+        String queryShop = "Select * from Shop where IdShop = ?";
+        try (PreparedStatement psStore = conn.prepareStatement(queryShop);){            
+            psStore.setInt(1, shopId);
+            try(ResultSet rsShop = psStore.executeQuery()){
+                
+                if(!rsShop.next()){
+                    return ret;
+                }                
+            }            
+        } catch (SQLException ex) {
+            Logger.getLogger(sa190222_ArticleOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        String query = "Select * from Article where IdShop = ? and Name = ?";               
+        try (PreparedStatement ps = conn.prepareStatement(query);){
+            
+            ps.setInt(1, shopId);
+            ps.setString(2, articleName);
+            
+            try(ResultSet rs = ps.executeQuery()){
+                
+                if(!rs.next()){
+                    String queryInsert = "Insert into Article(Name, Price, Count, IdShop) values(?, ?, 0, ?)";                    
+                    try (PreparedStatement psInsert = conn.prepareStatement(queryInsert, Statement.RETURN_GENERATED_KEYS);){
+                        
+                        psInsert.setString(1, articleName);
+                        psInsert.setInt(2, articlePrice);
+                        psInsert.setInt(3, shopId);
+                        
+                        try(ResultSet rsInsert = psInsert.executeQuery()){
+                            
+                            if(rsInsert.next()){
+                                ret = rsInsert.getInt(1);
+                            }
+                            
+                        } catch (SQLException ex) {
+                            Logger.getLogger(sa190222_ArticleOperations.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                    } catch (SQLException ex) {
+                        Logger.getLogger(sa190222_ArticleOperations.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(sa190222_ArticleOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return ret;
+    }
+    
+    public static void main(String[] args) {
+        
+        int ret = new sa190222_ArticleOperations().createArticle(1, "aaa", 100);
+        System.out.println("rs.etf.sab.student.sa190222_ArticleOperations.main() " + ret);
+        
     }
     
 }
